acc77b4752743df500af9aaa45a0d506502ccc1c
MR.DEV <notsolo045@gmail.com>
2026-01-21 20:08:39 +0500

    add keytool

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,13 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -31,13 +41,25 @@ android {
         versionName = flutter.versionName
     }
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as? String
+            keyPassword = keystoreProperties["keyPassword"] as? String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it.toString()) }  // ← SHUNDAY
+            storePassword = keystoreProperties["storePassword"] as? String
         }
     }
+
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")   // ← SHU YERNI O'ZGARTIRING!
+
+            isMinifyEnabled = false              // release uchun tavsiya etiladi
+            isShrinkResources = false           // proguard/minify bilan birga
+
+        }
+    }
+
 }
 
 dependencies {
